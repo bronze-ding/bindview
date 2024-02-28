5422c1ad73bc1fdd3477ce5ef42ea73c697ef0ce
灿烈 <3233834670@qq.com>
2024-02-28 17:19:58 +0800

    重构 use

--- src/core/use.js
+++ src/core/use.js
@@ -5,29 +5,25 @@ import BvWarn from "../tools/BvWarn"
  * @param {Function|Array<Function|Object>|Object} unit 插件或插件数组或普通对象
  */
 export default function Use(unit) {
-  let component = this
+  let Component = this
   if (typeof unit === 'function') {
-    unit(component)
+    unit(Component)
   } else if (Array.isArray(unit)) {
     for (let item of unit) {
       if (typeof item === 'function') {
-        item(component)
+        item(Component)
       } else {
-        if (typeof item.name === 'string' && Object.prototype.toString.call(item.unit) === '[object Object]') {
-          component.prototype[item.name] = item.unit
+        if (Object.prototype.toString.call(item) === '[object Object]' && typeof item._install_ === 'function') {
+          item._install_(Component)
         } else {
-          BvWarn(`对象插件应以 { name : String, unit : unitObject } 的方式注册`)
+          BvWarn(`对象插件是一个Bindview插件对象和函数`)
           continue
         }
       }
     }
-  } else if (Object.prototype.toString.call(unit) === '[object Object]') {
-    if (typeof unit.name === 'string' && Object.prototype.toString.call(unit.unit) === '[object Object]') {
-      component.prototype[unit.name] = unit.unit
-    } else {
-      BvWarn(`对象插件应以 { name:String,unit:unitObject } 的方式注册`)
-    }
+  } else if (Object.prototype.toString.call(unit) === '[object Object]' && typeof unit._install_ === 'function') {
+    unit._install_(Component)
   } else {
-    BvWarn(`插件应该为 Function 类型或 Array<Function> 类型或普通 Object 类型`)
+    BvWarn(`插件应该为 Function 类型或 Array<Function> 类型或Bindview插件对象`)
   }
 }
