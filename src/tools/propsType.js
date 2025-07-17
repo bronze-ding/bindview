8fece7a543082006e1131b073bf8f8c94e604838
灿烈 <3233834670@qq.com>
2025-07-17 23:47:16 +0800

    添加组件名输出

--- src/tools/propsType.js
+++ src/tools/propsType.js
@@ -14,31 +14,34 @@ function type(item) {
  * @param {Object} props props
  * @param {Object} config 类型配置
  * @returns {Object} props props
+ * @throws {String} 组件名
  */
-export default function propsType(props, config) {
-  if (Object.keys(config).length === 0) throw new BvError("propsType config 不能为空")
+export default function propsType(props, config, comName) {
+  const componentName = typeof comName === 'string' ? comName : '未设置'
+
+  if (Object.keys(config).length === 0) throw new BvError("propsType config 不能为空,当前组件：" + componentName)
 
   const z = /^(?:string|number|boolean|symbol|undefined|object|function|array)$/
 
   for (let i in config) {
     if (props[i] === undefined) {
-      BvWarn(`props 中无 ${i} 属性`)
+      BvWarn(`props 中无 ${i} 属性,当前组件：${componentName}`)
       continue
     }
     if (Array.isArray(config[i])) {
       for (let j of config[i]) {
         if (!z.test(j)) {
-          throw new BvError(`propsType config 的 ${i} 属性 ${j} 类型不正确`)
+          throw new BvError(`propsType config 的 ${i} 属性 ${j} 类型不正确,当前组件：${componentName}`)
         }
       }
       if (config[i].indexOf(type(props[i])) === -1) {
-        BvWarn(`props 中 ${i} 属性类型应该为 ${config[i]}`)
+        BvWarn(`props 中 ${i} 属性类型应该为 ${config[i]},当前组件：${componentName}`)
         continue
       }
     } else if (type(config[i]) === 'string') {
-      if (!z.test(config[i])) throw new BvError(`propsType config 的 ${i} 属性 ${config[i]} 类型不正确`)
+      if (!z.test(config[i])) throw new BvError(`propsType config 的 ${i} 属性 ${config[i]} 类型不正确,当前组件：${componentName}`)
       if (type(props[i]) !== config[i]) {
-        BvWarn(`props 中 ${i} 属性类型应该为 ${config[i]}`)
+        BvWarn(`props 中 ${i} 属性类型应该为 ${config[i]},当前组件：${componentName}`)
         continue
       }
     }
